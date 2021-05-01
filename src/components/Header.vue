7df40dd9ee0f024c220e407cc9b46470b0a1fcf1
Tarun Pai <tarun.pai@livspace.com>
2021-05-02 01:25:18 +0530

    Checking if issue is with logo.png

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <header class="text-gray-600 body-font">
       <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <img class="logo" src="@/assets/Logo.png" alt="Recipe Of Hope" srcset="" />
         <router-link to="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-secondary rounded-full" viewBox="0 0 24 24">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-secondary rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg> -->
+          </svg>
+          <!-- <img class="logo" src="@/assets/Logo.png" alt="Recipe Of Hope" srcset="" /> -->
           <span class="ml-3 text-xl text-primary">Recipe of Hope</span>
         </router-link>
         <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">

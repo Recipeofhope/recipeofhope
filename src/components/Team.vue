a3e621dd3a9096b1ff4a4d3ccd54ede9bf508ccb
Tarun Pai <tarun.pai@livspace.com>
2021-05-01 23:42:36 +0530

    Adding some more stuff.

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -8,7 +8,7 @@
       <div class="flex flex-wrap -m-2">
         <div class="p-2 lg:w-1/2 md:w-1/2 w-full">
           <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-            <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="../assets/Miti.png">
+            <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="@/assets/Miti.png">
             <div class="flex-grow">
               <h2 class="text-gray-900 title-font font-medium">Miti Desai</h2>
               <p class="text-gray-500">Founder and Cook</p>
@@ -17,7 +17,7 @@
         </div>
         <div class="p-2 lg:w-1/2 md:w-1/2 w-full">
           <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-            <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="../assets/Piyush.png">
+            <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="@/assets/Piyush.png">
             <div class="flex-grow">
               <h2 class="text-gray-900 title-font font-medium">Piyush Jain</h2>
               <p class="text-gray-500">Head of Operations</p>

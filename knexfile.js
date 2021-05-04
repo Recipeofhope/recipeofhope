ade2f72e4e934e58d483df4fa74dcb5943ab610f
tarunnurat <sharma.was.tarun@gmail.com>
2021-05-04 14:11:47 +0530

    Adding code to work with heroku's postgres.

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgresql://roh:recipes-of-hope@localhost:5432/recipesofhope',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations',
     },

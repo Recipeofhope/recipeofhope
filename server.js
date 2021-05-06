a595803c122ee996426ef1cdd0bc4424b377ddec
tarunnurat <sharma.was.tarun@gmail.com>
2021-05-06 15:06:10 +0530

    Creating API for all entities. (#4)
    * Adding API stubs.
    
    * Adding API stubs.

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
 
+app.use('/user', require('./routes/user'));
+app.use('/cook', require('./routes/cook'));
+app.use('/patient', require('./routes/patient'));
+app.use('/admin', require('./routes/admin'));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'))
-})
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});

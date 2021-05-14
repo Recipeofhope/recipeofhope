6b7ef12bb7c0cbe805720889671a79157c54e21d
Tarun Pai <tarun.pai@livspace.com>
2021-05-14 20:13:18 +0530

    intial commit for api integration

--- src/views/Cook.vue
+++ src/views/Cook.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container px-5 py-24 mx-auto">
-    <ProfileHeading class="-mt-10 mb-12" />
+    <ProfileHeading :name="cookDetails.name" class="-mt-10 mb-12" />
     <Stats class="my-20" />
     <Schedule />
     <Details />
@@ -12,12 +12,39 @@ import Details from '@/components/Details.vue'
 import Schedule from '@/components/Schedule.vue'
 import Stats from '@/components/Stats.vue'
 
+import axios from 'axios'
+
 export default {
+  data() {
+    return {
+      cookID: '123',
+      cookDetails: {}
+    }
+  },
   components: {
     ProfileHeading,
     Details,
     Schedule,
     Stats
+  },
+  async mounted() {
+    try {
+      const response = await this.getCookData();
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  methods: {
+    async getCookData() {
+      try {
+        const data = await axios.get(`/cook/${this.cookID}`);
+        this.cookDetails = data;
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 }
 </script>

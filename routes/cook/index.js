6b589bf3ed98ffa84f1c18601fc14e7b4d83f42d
tarunnurat <sharma.was.tarun@gmail.com>
2021-05-17 11:40:51 +0530

    Adding the meals ready API for the cooks.

--- routes/cook/index.js
+++ routes/cook/index.js
@@ -1,14 +1,13 @@
-const { scheduleMeals } = require('./cook');
+const { scheduleMeals, mealsReady } = require('./cook');
 
 var router = require('express').Router();
 
 router.put('/schedule', function(req, res) {
-  console.log('reached cook /schedule ');
   return scheduleMeals(req.decodedUser, req.body, res);
 });
 
-router.put('/update-meal/:id', function(req, res) {
-  console.log('reached cook update meal with id: ' + req.params.id);
+router.post('/meals-ready', function(req, res) {
+  return mealsReady(req.decodedUser, res);
 });
 
 module.exports = router;

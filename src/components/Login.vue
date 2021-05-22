95ed739a532c6934aab589cc8b8caafeafa5a7df
Siddharth Pant <siddharthpant92@gmail.com>
2021-05-22 08:56:26 -0600

    swapped login and signup

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,37 +6,30 @@
       <p class="leading-relaxed mt-4 text-secondary">Spreading Hope, One Meal at a Time.</p>
       </div>
       <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 shadow-md">
-        <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
+        <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Login</h2>
         <Field label="Full Name" name="fullname" type="text" :value.sync="fullname"></Field>
         <Field label="Phone Number" name="phoneNumber" type="tel" :value.sync="phoneNumber"></Field>
-        <button class="text-white bg-button border-0 py-2 px-8 focus:outline-none hover:bg-button rounded text-lg mt-10" @click="continueSignup()">Sign Up</button>
+        <button class="text-white bg-button border-0 py-2 px-8 focus:outline-none hover:bg-button rounded text-lg mt-10"  @click="loginClicked()">Login</button>
       </div>
     </div>
-    <Modal v-show="signup" @CloseModal="closeModal()" :name="fullname" :phone="phoneNumber" />
   </section>
 </template>
 <script>
 import Field from '@/components/Field.vue';
-import Modal from '@/components/Modal.vue';
 
 export default {
   data() {
     return {
-      signup: false,
       fullname: '',
       phoneNumber: ''
     }
   },
   components: {
-    Modal,
     Field
   },
   methods: {
-    continueSignup() {
-      this.signup = true
-    },
-    closeModal() {
-      this.signup = false
+    loginClicked() {
+      console.log("login button clicked")
     }
   }
 }

71dbd4d2b813d19475e2197c6df9ca99c8d952ee
harshitha-nano <harshitha@nanocorporation.co.uk>
2021-05-23 21:01:47 +0530

    Bug fixes for resposne catch bloxk & hide login screen from home screen if user logged in

--- src/utils/action-handler.js
+++ src/utils/action-handler.js
@@ -1,10 +1,11 @@
-export default async function asyncMiddleware (handler, commit, dispatch, id = null, toaster = false) {
+export default async function asyncMiddleware(handler, commit, dispatch, id = null, toaster = false) {
   dispatch('start', id, { root: true })
-  let res = null
+  let res = ''
   try {
     res = await handler()
     dispatch('handleSuccess', { type: id, res, toaster }, { root: true })
   } catch (error) {
+    res = error.response.data;
     dispatch('handleError', { type: id, error }, { root: true })
   }
   dispatch('end', id, { root: true })

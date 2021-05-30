fea2b55d449ccfa64dd948e178a3cac6dab02ca0
tarunnurat <sharma.was.tarun@gmail.com>
2021-05-31 04:39:21 +0530

    Adding vue-cli and changing .env files to use the correct prefix for getting the base url.

--- src/api/http/common.js
+++ src/api/http/common.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.BASE_URL;
+const baseUrl = process.env.VUE_APP_BASE_URL;
 const timeout = 600000;
 const headers = {};
 const token = localStorage.getItem('token');
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,54 +1,70 @@
-import auth from '../../api/collections/auth'
-import asyncMiddleware from '../../utils/action-handler'
+import auth from '../../api/collections/auth';
+import asyncMiddleware from '../../utils/action-handler';
 
 export default {
   namespaced: true,
   state: {
-    currentUser: localStorage.getItem('user') != null ? JSON.parse(localStorage.getItem('user')) : null,
+    currentUser:
+      localStorage.getItem('user') != null
+        ? JSON.parse(localStorage.getItem('user'))
+        : null,
     token: '',
-    userRole: ''
+    userRole: '',
   },
   getters: {
-    currentUser: state => state.currentUser
+    currentUser: (state) => state.currentUser,
   },
   mutations: {
     setUser(state, payload) {
-      state.currentUser = payload
+      state.currentUser = payload;
     },
     setToken(state, payload) {
-      state.token = payload
+      state.token = payload;
     },
     setUserRole(state, payload) {
-      state.userRole = payload
-    }
+      state.userRole = payload;
+    },
   },
   actions: {
-    async 'USER_LOGIN'({ commit, dispatch }, payload) {
-      const data = await asyncMiddleware(async function() {
-        localStorage.removeItem('token')
-        const { data } = await auth.login(payload)
-        if (data.auth === true) {
-          localStorage.setItem('user', JSON.stringify(data.user))
-          localStorage.setItem('token', data.access_token)
-          localStorage.setItem('role', data.user.user_type)
-          commit('setUser', JSON.stringify(data.user))
-          commit('setToken', data.access_token)
-        }
-        return data
-      }, commit, dispatch, 'USER_LOGIN', true)
+    async USER_LOGIN({ commit, dispatch }, payload) {
+      console.log('server baseURL: ' + process.env.VUE_APP_BASE_URL);
+      const data = await asyncMiddleware(
+        async function() {
+          localStorage.removeItem('token');
+          const { data } = await auth.login(payload);
+          if (data.auth === true) {
+            localStorage.setItem('user', JSON.stringify(data.user));
+            localStorage.setItem('token', data.access_token);
+            localStorage.setItem('role', data.user.user_type);
+            commit('setUser', JSON.stringify(data.user));
+            commit('setToken', data.access_token);
+          }
+          return data;
+        },
+        commit,
+        dispatch,
+        'USER_LOGIN',
+        true
+      );
 
-      return data
+      return data;
     },
     async LOGOUT({ commit, dispatch }, payload) {
-      const data = await asyncMiddleware(async function() {
-        const { data } = await auth.logout(payload)
-        localStorage.removeItem('user')
-        localStorage.removeItem('token')
-        localStorage.removeItem('role')
-        return data
-      }, commit, dispatch, 'LOGOUT', true)
+      const data = await asyncMiddleware(
+        async function() {
+          const { data } = await auth.logout(payload);
+          localStorage.removeItem('user');
+          localStorage.removeItem('token');
+          localStorage.removeItem('role');
+          return data;
+        },
+        commit,
+        dispatch,
+        'LOGOUT',
+        true
+      );
 
-      return data
-    }
-  }
-}
+      return data;
+    },
+  },
+};
